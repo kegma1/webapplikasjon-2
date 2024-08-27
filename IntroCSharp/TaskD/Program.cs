046af49472ab3bc36f9a07fdc4012a606fe4f541
Kennet Mathisen <kennetam02@gmail.com>
2024-08-27 10:01:39 +0200

    task d done

--- IntroCSharp/TaskD/Program.cs
+++ IntroCSharp/TaskD/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class TaskD {
+    private static void Main(string[] args) {
+        int x = Convert.ToInt32(args[0]);
+        int y = Convert.ToInt32(args[1]);
+
+        Swap(ref x, ref y);
+
+        Console.WriteLine($"x: {x} \ny: {y}");
+
+    }
+
+    static void Swap(ref int i, ref int j) {
+        int temp;
+        temp = i;
+        i = j;
+        j = temp;
+    }
+}
